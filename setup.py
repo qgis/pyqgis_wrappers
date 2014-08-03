f7540a5733a2eafe6a7ed92e6d4a9e47d583e047
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2014-08-03 19:22:36 +1000

    Checking setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='parfait',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/NathanW2/parfait',
+    license='GPL v2',
+    author='nathan.woodrow',
+    author_email='woodrow.nathan@gmail.com',
+    description='A more python style wrapper around pyqgis'
+)
