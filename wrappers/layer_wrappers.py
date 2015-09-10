9b60d82e335363f5ab48c9dc461ce3743c3002ae
Nathan Woodrow <madmanwoo@gmail.com>
2015-09-11 07:11:30 +1000

    Docs and map layers update

--- wrappers/layer_wrappers.py
+++ wrappers/layer_wrappers.py
@@ -3,12 +3,18 @@ from qgis.core import QgsMapLayerRegistry
 _layerreg = QgsMapLayerRegistry.instance()
 
 
-def map_layers(name=None):
+def map_layers(name=None, type=None):
     """
-    Return all the loaded layers.
+    Return all the loaded layers.  Filters by name (optional) first and then type (optional)
     :param name: (optional) name of layer to return..
+    :param type: (optional) The QgsMapLayer type of layer to return.
     :return: List of loaded layers. If name given will return all layers with matching name.
     """
+    layers = _layerreg.mapLayers().values()
+    _layers = []
     if name:
-        return _layerreg.mapLayersByName(name)
-    return _layerreg.mapLayers().values()
+        _layers = [layer for layer in layers if layer.layerName() == name]
+    if type:
+        _layers += [layer for layer in layers if layer.type() == type]
+
+    return _layers
--- wrappers/printing.py
+++ wrappers/printing.py
@@ -3,11 +3,21 @@ from qgis.core import QgsComposition
 
 
 class ComposerTemplate():
+    """
+    Contains methods for working with loading/saving QGIS composer template files
+    """
     def __init__(self, composition):
         self.composition = composition
 
     @classmethod
     def from_file(cls, template_path, mapsettings, data=None):
+        """
+        Create a template object from the given template path, map setttings, and data.
+        :param template_path: The template path.
+        :param mapsettings: QgsMapSettings used to render the map.
+        :param data: A dict of data to use for labels.
+        :return: A ComposerTemplate obect which wraps the created template.
+        """
         if not data:
             data = {}
 
@@ -21,17 +31,36 @@ class ComposerTemplate():
         return cls(composition)
 
     def __getitem__(self, item):
+        """
+        Return the composer item with the given name.
+
+        The same as doing getComposerItemById(item)
+        """
         return self.composition.getComposerItemById(item)
+
+    def export(self, outpath):
+        self.composition.refreshItems()
+        self.composition.exportAsPDF(outpath)
 
 
 def render_template(template_path, settings, canvas, outpath, data=None):
-    # You must set the id in the template
+    """
+    Render the template at the given path using the settings and the export to the output path.
+
+    Template assumes there is a map and legend objet in the template.
+    :param template_path: The path to the template.
+    :param settings: The QgsMapSettings used to render the template
+    :param canvas: QgsMapCanvas item used to render the template. (This is gross but needed for now)
+    :param outpath: The output path. Current only exports to PDF.
+    :param data: A dict of data to use for labels.
+    :return:
+    """
     template = ComposerTemplate.from_file(template_path, canvas.mapSettings(), data)
+
     map_item = template['map']
     map_item.setMapCanvas(canvas)
     map_item.zoomToExtent(settings.extent())
-    # # You must set the id in the template
+
     legend_item = template['legend']
     legend_item.updateLegend()
-    template.composition.refreshItems()
-    template.composition.exportAsPDF(outpath)
+    template.export(outpath)
