da5dc0c49ea04c88252f2b982a60e93012eea7fb
Nathan Woodrow <madmanwoo@gmail.com>
2015-09-27 20:09:27 +1000

    Add relative base project loading

--- parfait/projects.py
+++ parfait/projects.py
@@ -2,7 +2,7 @@ from wrappers.layer_wrappers import map_layers
 from qgis.core.contextmanagers import qgisapp
 from qgis.core import QgsProject, QgsMapLayerRegistry, QgsMapSettings
 from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge
-from PyQt4.QtCore import QFileInfo
+from PyQt4.QtCore import QFileInfo, QDir
 from PyQt4.QtXml import QDomDocument
 
 
@@ -19,18 +19,22 @@ class Project(object):
         return self
 
     @classmethod
-    def from_file(cls, filename, canvas):
+    def from_file(cls, filename, canvas, relative_base=None):
         """
         Load a project file from a path.
         :param filename: The path to the project file.
         :param canvas: (optional) Passing a canvas will auto add layers to the canvas when the load is
         loaded.
+        :param relative_base_path: (optional) Relative base path for the project file to load layers from
         :return: A Project object which wraps QgsProject.instance()
         """
         QgsProject.instance().clear()
         bridge = None
         if canvas:
             bridge = QgsLayerTreeMapCanvasBridge(QgsProject.instance().layerTreeRoot(), canvas)
+        if relative_base is None:
+            relative_base = os.path.dirname(filename)
+        QDir.setCurrent(relative_base)
         QgsProject.instance().read(QFileInfo(filename))
         if bridge:
             bridge.setCanvasLayers()
@@ -64,12 +68,13 @@ class Project(object):
         return settings
 
 
-def open_project(projectfile, canvas=None):
+def open_project(projectfile, canvas=None, relative_base_path=None):
     """
     Open a QGIS project file
     :param projectfile: The path to the project file to load.
     :param canvas: (optional) Canvas object.
+    :param relative_base_path: (optional) Relative base path for the project file to load layers from
     :return: A Project object wrapper with handy functions for doing project related stuff.
     """
-    return Project.from_file(projectfile, canvas)
+    return Project.from_file(projectfile, canvas, relative_base_path)
 
